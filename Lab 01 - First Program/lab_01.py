7b3b5448d56c4626a02cc3b63ce4fa47d8ce10d3
unknown <robbie.wagner@my.simpson.edu>
2020-08-27 15:31:50 -0500

    Work on lab 1

--- Lab 01 - First Program/lab_01.py
+++ Lab 01 - First Program/lab_01.py
@@ -1,4 +1,4 @@
-print("hello")
-print("This is a program")
-print("Lots of programmers like to program programs, and many programs are programmed by programmers")
-print("\"program\"")
+print("Hello.")
+print("This is a program.")
+print("Lots of programmers like to program programs, and many programs are programmed by programmers.")
+print("\"program.\"")
