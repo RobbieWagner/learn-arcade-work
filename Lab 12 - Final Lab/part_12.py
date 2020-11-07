cf0a400e54ed32560d25b478eb36b66dac25b55a
unknown <robbie.wagner@my.simpson.edu>
2020-11-07 10:06:13 -0600

    Work on Lab 12

--- Lab 12 - Final Lab/part_12.py
+++ Lab 12 - Final Lab/part_12.py
@@ -1,23 +1,24 @@
 import arcade
 import random
+import time
 
 """
 This code is adapted from the Array-Baked Grids lab
 """
 
 # Set how many rows and columns we will have
-ROW_COUNT = 20
-COLUMN_COUNT = 40
+ROW_COUNT = 3
+COLUMN_COUNT = 3
 
 # This sets the WIDTH and HEIGHT of each grid location
-WIDTH = 20
-HEIGHT = 20
+WIDTH = 100
+HEIGHT = 100
 
 BLANK_SPACE = 0
 
 # This sets the margin between each cell
 # and on the edges of the screen.
-MARGIN = 12
+MARGIN = 20
 
 # Do the math to figure out our screen dimensions
 SCREEN_WIDTH = (WIDTH + MARGIN) * COLUMN_COUNT + MARGIN + BLANK_SPACE
@@ -35,18 +36,41 @@ class MyGame(arcade.Window):
         """
         super().__init__(width, height)
 
-        self.mouse_pressed = False
+        self.clicks = 0
+        self.required_clicks = 1
         self.row_clicked = 0
         self.column_clicked = 0
+        self.return_value = 0
         self.grid = []
 
+        # initialization of the board
         for row in range(ROW_COUNT):
             self.grid.append([])
             for column in range(COLUMN_COUNT):
-                self.grid[row].append(0)
+                self.grid[row].append(10)
 
-        for i in range(random.randrange(5)):
-            self.grid[random.randrange(ROW_COUNT)][random.randrange(COLUMN_COUNT)] = 4
+        # gives each box a unique color
+        for row in range(ROW_COUNT):
+            for column in range(COLUMN_COUNT):
+                checking = True
+                some_value = 0
+
+                # picks a color, and assigns it if it is unique
+                while checking:
+                    color_value = random.randrange(9)
+                    print("color", color_value)
+                    for checking_row in range(ROW_COUNT):
+                        for checking_column in range(COLUMN_COUNT):
+                            if self.grid[checking_row][checking_column] != color_value:
+                                some_value += 1
+                                print(some_value)
+                            if some_value == COLUMN_COUNT * ROW_COUNT:
+                                print("color", color_value)
+                                checking = False
+                            elif checking_row == ROW_COUNT - 1 and checking_column == COLUMN_COUNT - 1:
+                                some_value = 0
+
+                self.grid[row][column] = color_value
 
         arcade.set_background_color(arcade.color.BLACK)
 
@@ -54,25 +78,64 @@ class MyGame(arcade.Window):
         self.grid_shape_list = None
         self.create_shapes_from_grid()
 
+    def on_update(self, delta_time: float):
+        if self.grid[0][0] == 10:
+            time.sleep(.5)
+
+            for row in range(ROW_COUNT):
+                for column in range(COLUMN_COUNT):
+                    checking = True
+                    some_value = 0
+
+                    # picks a color, and assigns it if it is unique
+                    while checking:
+                        color_value = random.randrange(9)
+                        print("color", color_value)
+                        for checking_row in range(ROW_COUNT):
+                            for checking_column in range(COLUMN_COUNT):
+                                if self.grid[checking_row][checking_column] != color_value:
+                                    some_value += 1
+                                    print(some_value)
+                                if some_value == COLUMN_COUNT * ROW_COUNT:
+                                    print("color", color_value)
+                                    checking = False
+                                elif checking_row == ROW_COUNT - 1 and checking_column == COLUMN_COUNT - 1:
+                                    some_value = 0
+
+                    self.grid[row][column] = color_value
+
+            self.create_shapes_from_grid()
+
     def create_shapes_from_grid(self):
-        """ This creates a list of buffered shapes, and loads the
-        rectangles into that list for faster display. """
         self.grid_shape_list = arcade.ShapeElementList()
 
+        # makes each box
         for row in range(ROW_COUNT):
             for column in range(COLUMN_COUNT):
 
                 # Figure out what color to draw the box
-                if self.grid[row][column] == 2:
-                    color = arcade.color.WHITE
+                if self.grid[row][column] == 0:
+                    color = arcade.color.SALMON
                 elif self.grid[row][column] == 1:
-                    color = arcade.color.GREEN
-                elif self.grid[row][column] == 0:
-                    color = (random.randrange(70), random.randrange(70), random.randrange(70))
-                elif self.grid[row][column] == 3:
                     color = arcade.color.GRAY
+                elif self.grid[row][column] == 2:
+                    color = arcade.color.BLUE
+                elif self.grid[row][column] == 3:
+                    color = arcade.color.YELLOW
                 elif self.grid[row][column] == 4:
-                    color = (random.randrange(100, 200), random.randrange(100, 200), random.randrange(100, 200))
+                    color = arcade.color.PURPLE
+                elif self.grid[row][column] == 5:
+                    color = arcade.color.ORANGE
+                elif self.grid[row][column] == 6:
+                    color = arcade.color.AMAZON
+                elif self.grid[row][column] == 7:
+                    color = arcade.color.OLIVE
+                elif self.grid[row][column] == 8:
+                    color = arcade.color.AQUA
+                elif self.grid[row][column] == 9:
+                    color = arcade.color.GREEN
+                elif self.grid[row][column] == 10:
+                    color = arcade.color.WHITE
 
                 # Figure where to put the box
                 x = (MARGIN + WIDTH) * column + MARGIN + WIDTH // 2
@@ -92,175 +155,39 @@ class MyGame(arcade.Window):
 
     def on_mouse_press(self, x, y, button, modifiers):
         """ Called when the user presses a mouse button. """
-        self.mouse_pressed = True
         # Change the x/y screen coordinates to grid coordinates
         self.column_clicked = int(x // (WIDTH + MARGIN))
         self.row_clicked = int(y // (HEIGHT + MARGIN))
 
         print(f"Click coordinates: ({x}, {y}). Grid coordinates: ({self.row_clicked}, {self.column_clicked})")
 
-        # Make sure we are on-grid. It is possible to click in the upper right
-        # corner in the margin and go to a grid location that doesn't exist
+        # this assures that nothing happens unless they click somewhere on the grid
         if self.row_clicked < ROW_COUNT and self.column_clicked < COLUMN_COUNT:
 
-            # Highlight center
-            if self.grid[self.row_clicked][self.column_clicked] == 0 \
-                    or self.grid[self.row_clicked][self.column_clicked] == 4:
-                self.grid[self.row_clicked][self.column_clicked] = 1
-            elif self.grid[self.row_clicked][self.column_clicked] == 2:
-                self.grid[self.row_clicked][self.column_clicked] = 3
+            # Highlight the box they clicked
+            self.return_value = self.grid[self.row_clicked][self.column_clicked]
+            self.grid[self.row_clicked][self.column_clicked] = 9
 
-            # Highlight bottom left
-            if self.row_clicked > 0 \
-                    and self.column_clicked > 0 \
-                    and (self.grid[self.row_clicked - 1][self.column_clicked - 1] == 0
-                         or self.grid[self.row_clicked - 1][self.column_clicked - 1] == 4):
-                self.grid[self.row_clicked - 1][self.column_clicked - 1] = 1
-            elif self.row_clicked > 0 \
-                    and self.column_clicked > 0 \
-                    and self.grid[self.row_clicked - 1][self.column_clicked - 1] == 2:
-                self.grid[self.row_clicked - 1][self.column_clicked - 1] = 3
-
-            # Highlight bottom middle
-            if self.row_clicked > 0 \
-                    and (self.grid[self.row_clicked-1][self.column_clicked] == 0
-                         or self.grid[self.row_clicked - 1][self.column_clicked] == 4):
-                self.grid[self.row_clicked-1][self.column_clicked] = 1
-            elif self.row_clicked > 0 \
-                    and self.grid[self.row_clicked-1][self.column_clicked] == 2:
-                self.grid[self.row_clicked-1][self.column_clicked] = 3
-
-            # Highlight bottom right
-            if self.row_clicked > 0 \
-                    and self.column_clicked < COLUMN_COUNT - 1 \
-                    and (self.grid[self.row_clicked - 1][self.column_clicked + 1] == 0
-                         or self.grid[self.row_clicked - 1][self.column_clicked + 1] == 4):
-                self.grid[self.row_clicked - 1][self.column_clicked + 1] = 1
-            elif self.row_clicked > 0 \
-                    and self.column_clicked < COLUMN_COUNT - 1 \
-                    and self.grid[self.row_clicked - 1][self.column_clicked + 1] == 2:
-                self.grid[self.row_clicked - 1][self.column_clicked + 1] = 3
-
-            # Highlight center right
-            if self.column_clicked < COLUMN_COUNT - 1 \
-                    and (self.grid[self.row_clicked][self.column_clicked + 1] == 0
-                         or self.grid[self.row_clicked][self.column_clicked + 1] == 4):
-                self.grid[self.row_clicked][self.column_clicked + 1] = 1
-            elif self.column_clicked < COLUMN_COUNT - 1 \
-                    and self.grid[self.row_clicked][self.column_clicked + 1] == 2:
-                self.grid[self.row_clicked][self.column_clicked + 1] = 3
-
-            # Highlight center left
-            if self.column_clicked > 0 \
-                    and (self.grid[self.row_clicked][self.column_clicked - 1] == 0
-                         or self.grid[self.row_clicked][self.column_clicked - 1] == 4):
-                self.grid[self.row_clicked][self.column_clicked - 1] = 1
-            elif self.column_clicked > 0 \
-                    and self.grid[self.row_clicked][self.column_clicked - 1] == 2:
-                self.grid[self.row_clicked][self.column_clicked - 1] = 3
-
-            # Highlight top left
-            if self.row_clicked < ROW_COUNT - 1 \
-                    and self.column_clicked > 0 \
-                    and (self.grid[self.row_clicked + 1][self.column_clicked - 1] == 0
-                         or self.grid[self.row_clicked + 1][self.column_clicked - 1] == 4):
-                self.grid[self.row_clicked + 1][self.column_clicked - 1] = 1
-            elif self.row_clicked < ROW_COUNT - 1 \
-                    and self.column_clicked > 0 \
-                    and self.grid[self.row_clicked + 1][self.column_clicked - 1] == 2:
-                self.grid[self.row_clicked + 1][self.column_clicked - 1] = 3
-
-            # Highlight top middle
-            if self.row_clicked < ROW_COUNT - 1 \
-                    and (self.grid[self.row_clicked + 1][self.column_clicked] == 0
-                         or self.grid[self.row_clicked + 1][self.column_clicked] == 4):
-                self.grid[self.row_clicked + 1][self.column_clicked] = 1
-            elif self.row_clicked < ROW_COUNT - 1 \
-                    and self.grid[self.row_clicked + 1][self.column_clicked] == 2:
-                self.grid[self.row_clicked + 1][self.column_clicked] = 3
-
-            # Highlight top right
-            if self.row_clicked < ROW_COUNT - 1 \
-                    and self.column_clicked < COLUMN_COUNT - 1 \
-                    and (self.grid[self.row_clicked + 1][self.column_clicked + 1] == 0
-                         or self.grid[self.row_clicked + 1][self.column_clicked + 1] == 4):
-                self.grid[self.row_clicked + 1][self.column_clicked + 1] = 1
-            elif self.row_clicked < ROW_COUNT - 1 \
-                    and self.column_clicked < COLUMN_COUNT - 1 \
-                    and self.grid[self.row_clicked + 1][self.column_clicked + 1] == 2:
-                self.grid[self.row_clicked + 1][self.column_clicked + 1] = 3
+            # checks if they finished the level
+            self.clicks += 1
 
         self.create_shapes_from_grid()
 
     def on_mouse_release(self, x, y, button, modifiers):
 
-        self.mouse_pressed = False
-
         print(f"Release coordinates: ({self.row_clicked}, {self.column_clicked})")
 
         if self.row_clicked < ROW_COUNT and self.column_clicked < COLUMN_COUNT:
 
-            # Flip center
-            if self.grid[self.row_clicked][self.column_clicked] == 1:
-                self.grid[self.row_clicked][self.column_clicked] = 2
-            elif self.grid[self.row_clicked][self.column_clicked] == 3:
-                self.grid[self.row_clicked][self.column_clicked] = 0
+            self.grid[self.row_clicked][self.column_clicked] = self.return_value
 
-            # Flip bottom left
-            if self.row_clicked > 0 and self.column_clicked > 0 \
-                    and self.grid[self.row_clicked-1][self.column_clicked-1] == 1:
-                self.grid[self.row_clicked-1][self.column_clicked-1] = 2
-            elif self.row_clicked > 0 and self.column_clicked > 0 \
-                    and self.grid[self.row_clicked-1][self.column_clicked-1] == 3:
-                self.grid[self.row_clicked - 1][self.column_clicked - 1] = 0
-
-            # Flip bottom middle
-            if self.row_clicked > 0 and self.grid[self.row_clicked - 1][self.column_clicked] == 1:
-                self.grid[self.row_clicked - 1][self.column_clicked] = 2
-            elif self.row_clicked > 0 and self.grid[self.row_clicked - 1][self.column_clicked] == 3:
-                self.grid[self.row_clicked - 1][self.column_clicked] = 0
-
-            # Flip bottom right
-            if self.row_clicked > 0 and self.column_clicked < COLUMN_COUNT - 1 \
-                    and self.grid[self.row_clicked - 1][self.column_clicked + 1] == 1:
-                self.grid[self.row_clicked - 1][self.column_clicked + 1] = 2
-            elif self.row_clicked > 0 and self.column_clicked < COLUMN_COUNT - 1 \
-                    and self.grid[self.row_clicked - 1][self.column_clicked + 1] == 3:
-                self.grid[self.row_clicked - 1][self.column_clicked + 1] = 0
-
-            # Flip center right
-            if self.column_clicked < COLUMN_COUNT - 1 and self.grid[self.row_clicked][self.column_clicked + 1] == 1:
-                self.grid[self.row_clicked][self.column_clicked + 1] = 2
-            elif self.column_clicked < COLUMN_COUNT - 1 and self.grid[self.row_clicked][self.column_clicked + 1] == 3:
-                self.grid[self.row_clicked][self.column_clicked + 1] = 0
-
-            # Flip center left
-            if self.column_clicked > 0 and self.grid[self.row_clicked][self.column_clicked - 1] == 1:
-                self.grid[self.row_clicked][self.column_clicked - 1] = 2
-            elif self.column_clicked > 0 and self.grid[self.row_clicked][self.column_clicked - 1] == 3:
-                self.grid[self.row_clicked][self.column_clicked - 1] = 0
-
-            # Flip top left
-            if self.row_clicked < ROW_COUNT - 1 and self.column_clicked > 0 \
-                    and self.grid[self.row_clicked + 1][self.column_clicked - 1] == 1:
-                self.grid[self.row_clicked + 1][self.column_clicked - 1] = 2
-            elif self.row_clicked < ROW_COUNT - 1 and self.column_clicked > 0 \
-                    and self.grid[self.row_clicked + 1][self.column_clicked - 1] == 3:
-                self.grid[self.row_clicked + 1][self.column_clicked - 1] = 0
-
-            # Flip top middle
-            if self.row_clicked < ROW_COUNT - 1 and self.grid[self.row_clicked + 1][self.column_clicked] == 1:
-                self.grid[self.row_clicked + 1][self.column_clicked] = 2
-            elif self.row_clicked < ROW_COUNT - 1 and self.grid[self.row_clicked + 1][self.column_clicked] == 3:
-                self.grid[self.row_clicked + 1][self.column_clicked] = 0
-
-            # Flip top right
-            if self.row_clicked < ROW_COUNT - 1 and self.column_clicked < COLUMN_COUNT - 1 \
-                    and self.grid[self.row_clicked + 1][self.column_clicked + 1] == 1:
-                self.grid[self.row_clicked + 1][self.column_clicked + 1] = 2
-            elif self.row_clicked < ROW_COUNT - 1 and self.column_clicked < COLUMN_COUNT - 1 \
-                    and self.grid[self.row_clicked + 1][self.column_clicked + 1] == 3:
-                self.grid[self.row_clicked + 1][self.column_clicked + 1] = 0
+            # turns the grid to white and
+            if self.clicks == self.required_clicks:
+                for row in range(ROW_COUNT):
+                    for column in range(COLUMN_COUNT):
+                        self.grid[row][column] = 10
+                self.clicks = 0
+                self.required_clicks += 1
 
         self.create_shapes_from_grid()
 
